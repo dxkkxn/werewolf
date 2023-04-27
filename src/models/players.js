f8b8c055ac180047c81be736968cda86b53617a7
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-28 00:43:06 +0200

    players model modif added autoincrement

--- src/models/players.js
+++ src/models/players.js
@@ -1,11 +1,28 @@
+const Sequelize = require('sequelize');
 const db = require('./database.js');
 const games = require('./games.js');
 const users = require('./users.js');
 
 const players = db.define(
   'players',
-  {},
-  { timestamps: false }
+  {
+    idPlayer: {
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true
+    }
+  },
+  {
+    timestamps: false,
+    indexes: [
+      {
+        unique: true,
+        fields: ['idGame', 'userName']
+      }
+    ]
+  }
 );
 games.belongsToMany(users, { foreignKey: 'idGame', primaryKey: true, through: 'players' });
 users.belongsToMany(games, { foreignKey: 'userName', primaryKey: true, through: 'players' });
