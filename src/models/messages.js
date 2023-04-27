5400a8e25f2b5abb01f6b629657f2e652e3e63ff
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-28 01:24:22 +0200

    messages model finished

---
+++ src/models/messages.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+const db = require('./database.js');
+const playersInGame = require('./playersInGame.js');
+
+const messages = db.define(
+  'messages',
+  {
+    idMessage: {
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    time: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    body: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    current: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    gameTime: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [['day', 'night']],
+          msg: 'gameTime can only be day or night'
+        }
+      }
+    }
+  },
+  { timestamps: false }
+);
+playersInGame.hasOne(messages, { foreignKey: 'idPlayer', primaryKey: true });
+
+module.exports = messages;
