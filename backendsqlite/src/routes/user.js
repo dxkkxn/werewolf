1f41092827b7018741d549327adfbeed5f29b1b7
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 01:09:28 +0200

    renamed validators in userValidators

--- backendsqlite/src/routes/user.js
+++ backendsqlite/src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { checkUser, addUser } = require('../controllers/user.js');
-const { validateAddUser, validateBody } = require('../middlewares/validators.js');
+const { validateAddUser, validateBody } = require('../middlewares/userValidators.js');
 
 router.post('/signin', validateBody, validateAddUser, addUser);
 router.post('/login', validateBody, checkUser);
