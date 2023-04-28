cd75b3eb960b87c4ac5af87bac0e05fe421c1b15
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-28 22:47:05 +0200

    check user

--- backendsqlite/src/controllers/user.js
+++ backendsqlite/src/controllers/user.js
@@ -1,61 +1,53 @@
-const users = require("../models/users.js");
-const status = require("http-status");
-const has = require("has-keys");
-const CodeError = require("../util/CodeError.js");
-const bcrypt = require("bcrypt");
-const jws = require("jws");
-require("mandatoryenv").load(["TOKENSECRET"]);
-const { TOKENSECRET } = process.env;
-const validator = require("validator");
+const users = require('../models/users.js');
+const status = require('http-status');
+// const has = require('has-keys');
+const CodeError = require('../util/CodeError.js');
+const bcrypt = require('bcrypt');
+// const jws = require('jws');
+// const validator = require('validator');
 
-const getUsers = async (req, res) => {
-  try {
-    const data = await users.findAll({
-      attributes: ["id", "username", "password"],
-    });
-    res.json({ status: true, message: "returning users", data });
-  } catch (error) {
-    res.send({ status: false, message: error.message });
-  }
-};
-const addUser = async (req, res) => {
-  try {
-    const { username, password, confirmPassword } = JSON.parse(req.body.data);
-    console.log(username, password);
-    //encryptin password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    await users.create({
-      username: username,
-      password: hashedPassword,
-    });
-    res.json({ status: true, message: "user Added" });
-  } catch (error) {
-    res.send({ status: false, message: error.message });
-  }
-};
-// const updateUser = async (req,res) => {
+// const getUsers = async (req, res) => {
 //   try {
-//     const {username,newUsername} = JSON.parse(req.body.data);
-//     await users.update({
-//       username:username,
-//       where:{username}
-//     })
+//     const data = await users.findAll({
+//       attributes: ['id', 'username', 'password'],
+//     });
+//     res.json({ status: true, message: 'returning users', data });
 //   } catch (error) {
 //     res.send({ status: false, message: error.message });
 //   }
 // };
-//TODO
-// const deleteUser = async (req,res) =>{
-//   try {
-//     await users.
-//   } catch (error) {
-//
-//   }
-// }
+
+const addUser = async (req, res) => {
+  try {
+    const { username, password } = req;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await users.create({ username, password: hashedPassword });
+    res.status(status.CREATED).json({ message: 'user added' });
+  } catch (error) {
+    res.status(status.INTERNAL_ERROR).json({ message: 'internal error' });
+  }
+};
+
+const checkUser = async (req, res) => {
+  const { username, password } = req;
+  const user = await users.findOne(
+    {
+      where: { username },
+      attributes: ['username', 'password']
+    });
+  const [usernameDB, passwordDB] = [user.username, user.password];
+  if (!usernameDB) {
+    throw new CodeError('not existing user', status.UNAUTHORIZED);
+  }
+  const result = await bcrypt.compare(password, passwordDB);
+  if (!result) {
+    throw new CodeError('passwords dont match', status.UNAUTHORIZED);
+  }
+  res.status(status.OK).json({ message: 'logged succesfully', token: 'abcd' });
+};
+
 module.exports = {
-  getUsers,
   addUser,
-  // updateUser,
-  // deleteUser,
+  checkUser
 };
