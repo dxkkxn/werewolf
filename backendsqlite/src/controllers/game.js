71eca990c7794784c525984e12e75084000de8ad
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 14:42:05 +0200

    controllers/game: adding joinGame

--- backendsqlite/src/controllers/game.js
+++ backendsqlite/src/controllers/game.js
@@ -2,9 +2,10 @@ const games = require('../models/games.js');
 const status = require('http-status');
 const has = require('has-keys');
 const CodeError = require('../util/CodeError.js');
+const players = require('../models/players.js');
 
 const createGame = async (req, res) => {
-  const creatorUsername = req.params.username;
+  const creatorUsername = req.username;
   const { minPlayers, maxPlayers, dayDuration, nightDuration, werewolfProbability } = JSON.parse(req.body.data);
   await games.create({ creatorUsername, minPlayers, maxPlayers, dayDuration, nightDuration, werewolfProbability });
   res.status(status.CREATED).json({ message: 'game created' });
@@ -20,7 +21,10 @@ const getStateGame = async (req, res) => {
 };
 
 const joinGame = async (req, res) => {
-  throw new CodeError('not implemented yet', status.NOT_IMPLEMENTED);
+  const username = req.username;
+  const idGame = req.params.idGame;
+  await players.create({ username, idGame });
+  res.status(status.OK).json({ message: `user: ${username} joined game with id: ${idGame}` });
 };
 
 const addMessage = async (req, res) => {
