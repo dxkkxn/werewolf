1d80dafe6fe8ee16f6c875e02cfc0cf344a5430e
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 02:27:09 +0200

    gameValidators: modif requiredAttrs
    if default values exits not all properties need to be passed

--- backendsqlite/src/middlewares/gameValidators.js
+++ backendsqlite/src/middlewares/gameValidators.js
@@ -3,15 +3,20 @@ const has = require('has-keys');
 const CodeError = require('../util/CodeError.js');
 
 const validateBodyCreateGame = async (req, res, next) => {
-  const requiredAttrs = ['minPlayers', 'maxPlayers', 'dayDuration', 'nightDuration', 'werewolfProbability'];
+  if (!has(req.body, ['data'])) {
+    throw new CodeError('You must include a data property in the request body', status.BAD_REQUEST);
+  }
+  const data = JSON.parse(req.body.data);
+  // const requiredAttrs = ['minPlayers', 'maxPlayers', 'dayDuration', 'nightDuration', 'werewolfProbability'];
+  const requiredAttrs = ['dayDuration', 'nightDuration'];
   const notFoundAttrs = [];
   requiredAttrs.forEach((attr) => {
-    if (!has(req.body, [attr])) {
+    if (!has(data, [attr])) {
       notFoundAttrs.push(attr);
     }
   });
   if (notFoundAttrs.length !== 0) {
-    throw new CodeError(`need attributes: ${notFoundAttrs} where not found`, status.BAD_REQUEST);
+    throw new CodeError(`needed attributes: [${notFoundAttrs}] where not found`, status.BAD_REQUEST);
   }
   next();
 };
