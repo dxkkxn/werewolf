6931992eba6813ff311271e73562d615c019d045
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 14:40:31 +0200

    simplified routes

--- backendsqlite/src/routes/game.js
+++ backendsqlite/src/routes/game.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { createGame, getGames, getStateGame, joinGame, addMessage } = require('../controllers/game.js');
 const { validateToken } = require('../middlewares/userValidators.js');
-const { validateBodyCreateGame, validateUserInGame } = require('../middlewares/gameValidators.js');
+const { validateBodyCreateGame, validateUserInGame, validateIdGame } = require('../middlewares/gameValidators.js');
 
-router.get('/:username/game', validateToken, getGames);
-router.post('/:username/game', validateToken, validateBodyCreateGame, createGame);
+router.get('/game', validateToken, getGames);
+router.post('/game', validateToken, validateBodyCreateGame, createGame);
 
-// router.post('/:username/:idGame', validateToken, joinGame);
+router.post('/game/:idGame', validateToken, validateIdGame, joinGame);
 // router.get('/:username/:idGame', validateToken, validateUserInGame, getStateGame);
 
 // router.post('/:username/:idGame/message', validateToken, validateUserInGame, addMessage);
