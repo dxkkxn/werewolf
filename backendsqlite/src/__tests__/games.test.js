e2111244923ef6e59f936a60117b0b2293ce3b9c
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 02:28:27 +0200

    more game tests

--- backendsqlite/src/__tests__/games.test.js
+++ backendsqlite/src/__tests__/games.test.js
@@ -2,6 +2,7 @@ const app = require('../app');
 const request = require('supertest');
 const status = require('http-status');
 
+/* eslint-env jest */
 let token;
 describe('create test user for this module', () => {
   test('create testGame user', async () => {
@@ -28,5 +29,36 @@ describe('create game', () => {
       .post('/testGame/game')
       .set({ 'x-access-token': token });
     expect(response.statusCode).toBe(status.BAD_REQUEST);
+    expect(response.body.message).toBe('You must include a data property in the request body');
+  });
+  test('not passed required properties', async () => {
+    const response = await request(app)
+      .post('/testGame/game')
+      .set({ 'x-access-token': token })
+      .send({ data: '{}' });
+    expect(response.statusCode).toBe(status.BAD_REQUEST);
+    const notFoundAttrs = ['dayDuration', 'nightDuration'];
+    expect(response.body.message).toBe(`needed attributes: [${notFoundAttrs}] where not found`);
+  });
+  test('creating a normal game', async () => {
+    const data = {creatorUsername: 'testGame', dayDuration: 3, nightDuration: 2};
+    const response = await request(app)
+      .post('/testGame/game')
+      .set({ 'x-access-token': token })
+      .send({ data: JSON.stringify(data) });
+    expect(response.statusCode).toBe(status.CREATED);
+    // const notFoundAttrs = ['minPlayers', 'maxPlayers', 'dayDuration', 'nightDuration', 'werewolfProbability'];
+    expect(response.body.message).toBe('game created');
+  });
+});
+
+describe('get games', () => {
+  test('getting all existing games', async () => {
+    const response = await request(app)
+      .get('/testGame/game')
+      .set({ 'x-access-token': token });
+    expect(response.statusCode).toBe(status.OK);
+    expect(response.body.message).toBe('returning games in the data property');
+    expect(response.body.data).toBe('[{"idGame":1,"minPlayers":5,"maxPlayers":20,"dayDuration":3,"nightDuration":2,"werewolfProbability":0.33,"creatorUsername":"testGame"}]');
   });
 });
