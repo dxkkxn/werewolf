3d1f416aac2d92a94b7f9367658d7ef2611b9c72
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-04-29 15:50:05 +0200

    games test: test of new getGames

--- backendsqlite/src/__tests__/games.test.js
+++ backendsqlite/src/__tests__/games.test.js
@@ -41,7 +41,7 @@ describe('create game', () => {
     expect(response.body.message).toBe(`needed attributes: [${notFoundAttrs}] where not found`);
   });
   test('creating a normal game', async () => {
-    const data = {creatorUsername: 'testGame', dayDuration: 3, nightDuration: 2};
+    const data = { creatorUsername: 'testGame', dayDuration: 3, nightDuration: 2 };
     const response = await request(app)
       .post('/game')
       .set({ 'x-access-token': token })
@@ -58,11 +58,21 @@ describe('get games', () => {
       .get('/game')
       .set({ 'x-access-token': token });
     expect(response.body.message).toBe('returning games in the data property');
-    expect(response.body.data).toBe('[{"idGame":1,"minPlayers":5,"maxPlayers":20,"dayDuration":3,"nightDuration":2,"werewolfProbability":0.33,"creatorUsername":"testGame"}]');
+    const data = JSON.parse(response.body.data);
+    expect(data.length).toBe(1);
+    expect(data[0]).toEqual({
+      idGame: 1,
+      minPlayers: 5,
+      maxPlayers: 20,
+      dayDuration: 3,
+      nightDuration: 2,
+      werewolfProbability: 0.33,
+      creatorUsername: 'testGame',
+      players: []
+    });
     expect(response.statusCode).toBe(status.OK);
   });
 });
-
 
 let token2;
 describe('create test user 2 for this module', () => {
@@ -88,9 +98,27 @@ describe('join game', () => {
   test('testGame2 joins game 1 created by testgame', async () => {
     const response = await request(app)
       .post('/game/1')
-      .set({'x-access-token': token2})
+      .set({ 'x-access-token': token2 });
     expect(response.body.message).toBe('user: testGame2 joined game with id: 1');
     expect(response.statusCode).toBe(status.OK);
   });
-
+  test('verif testGame2 is a player of game 1', async () => {
+    const response = await request(app)
+      .get('/game')
+      .set({ 'x-access-token': token });
+    expect(response.body.message).toBe('returning games in the data property');
+    const data = JSON.parse(response.body.data);
+    expect(data.length).toBe(1);
+    expect(data[0]).toEqual({
+      idGame: 1,
+      minPlayers: 5,
+      maxPlayers: 20,
+      dayDuration: 3,
+      nightDuration: 2,
+      werewolfProbability: 0.33,
+      creatorUsername: 'testGame',
+      players: ['testGame2']
+    });
+    expect(response.statusCode).toBe(status.OK);
+  });
 });
