d541e372d23b3726338bce74eedb7f64aa685fc7
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-05-12 14:08:11 +0200

    routes/game.js: added validateUserNotAlreadyInGame

--- backendsqlite/src/routes/game.js
+++ backendsqlite/src/routes/game.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createGame, getGames, getGameWithId, joinGame, addMessage, startGame, getStateOfGame } = require('../controllers/game.js');
+const {
+  createGame, getGames, getGameWithId,
+  joinGame, addMessage, startGame, getStateOfGame
+} = require('../controllers/game.js');
 const { validateToken } = require('../middlewares/userValidators.js');
-const { validateGameNotStarted, validateBodyCreateGame, validateUserInGame, validateIdGame, validateUserIsCreator, validateGameStarted } = require('../middlewares/gameValidators.js');
+const {
+  validateGameNotStarted, validateBodyCreateGame, validateUserInGame,
+  validateIdGame, validateUserIsCreator, validateGameStarted,
+  validateUserNotAlreadyInGame
+} = require('../middlewares/gameValidators.js');
 
 router.get('/game', validateToken, getGames);
-router.post('/game', validateToken, validateBodyCreateGame, createGame);
+router.post('/game', validateToken, validateBodyCreateGame, validateUserNotAlreadyInGame, createGame);
 
-router.post('/game/:idGame', validateToken, validateIdGame, joinGame);
+router.post('/game/:idGame', validateToken, validateIdGame, validateUserNotAlreadyInGame, joinGame);
 router.get('/game/:idGame', validateToken, validateIdGame, validateUserInGame, getGameWithId);
 
 router.post('/game/:idGame/play', validateToken, validateIdGame, validateUserInGame, validateUserIsCreator, validateGameNotStarted, startGame);
