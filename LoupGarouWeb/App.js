49d3a663041e65f7b548a59aff0e3942fa6e8c72
Youssef Benjelloun El Kbibi <youssef.benjelloun-el-kbibi@grenoble-inp.org>
2023-05-15 18:30:26 +0200

    app.js: linter setup

--- LoupGarouWeb/App.js
+++ LoupGarouWeb/App.js
@@ -5,55 +5,55 @@ import { MainNavigator } from './components/MainNavigator';
 import LockScreen from './components/LockScreen.js';
 import {useEffect} from 'react';
 const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: '#371b58',
-  },
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: '#371b58',
+    },
 };
 
 export default function App() {
-  // useEffect(() => {
-  //   document.body.style.display='display-flex';
-  // });
-  // const gameProps = {
-  //   hoursToStart: '5',
-  //   nbWolves: '3',
-  //   nbPlayersMin: '9',
-  //   probaC: '0.3',
-  //   probaI: '0.2',
-  //   probaV: '0',
-  //   probaS: '.7',
-  //   dayStart: '8',
-  //   dayEnd: '22',
-  //   avatarId:'4',
-  //   creatorPseudo:"notawolf",
-  // };
-  return (
+    // useEffect(() => {
+    //   document.body.style.display='display-flex';
+    // });
+    // const gameProps = {
+    //   hoursToStart: '5',
+    //   nbWolves: '3',
+    //   nbPlayersMin: '9',
+    //   probaC: '0.3',
+    //   probaI: '0.2',
+    //   probaV: '0',
+    //   probaS: '.7',
+    //   dayStart: '8',
+    //   dayEnd: '22',
+    //   avatarId:'4',
+    //   creatorPseudo:"notawolf",
+    // };
+    return (
     // <View style={styles.container}>
     //   <LockScreen />
     // </View>
-    <View style={styles.container}>
-      <NavigationContainer style={styles.navContainer} theme={MyTheme} >
-        <MainNavigator />
-      </NavigationContainer>
-    </View>
-  );
+        <View style={styles.container}>
+            <NavigationContainer style={styles.navContainer} theme={MyTheme} >
+                <MainNavigator />
+            </NavigationContainer>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: '#371b58',
+    container: {
+        flex:1,
+        backgroundColor: '#371b58',
     // alignItems: 'center',
     // justifyContent: 'center',
-  },
+    },
 
-  navContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    navContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 
